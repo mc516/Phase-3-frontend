60ba84f9371760c4bae27cdb82fd18b91dc53d82
Mickey <mickeyc11111@gmail.com>
2022-10-08 16:51:59 -0400

    react router dom v6 and home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Home from './Home';
+import {Routes, Route} from "react-router-dom"
+import {useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:9292/restaurants")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }, [])
+    
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+          
+       
+      </Routes>
+      
      
     </div>
   );
