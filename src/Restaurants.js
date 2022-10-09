c6a3dfbc4df5dce7e389fd977f8a81524c499a64
Mickey <mickeyc11111@gmail.com>
2022-10-08 20:13:46 -0400

    NreRestaurant component and add restaurant button

--- src/Restaurants.js
+++ src/Restaurants.js
@@ -1,15 +1,23 @@
 import React from "react";
 import RestaurantCard from "./RestaurantCard";
+import NewRestaurant from   "./NewRestaurant"
 import { useState, useEffect} from 'react'
 
 function Restaurants() {
     const [restaurants, setRestaurants] = useState([])
+    const [isClicked, setIsClicked] = useState(false)
+
     useEffect(() => {
         fetch("http://localhost:9292/restaurants")
         .then(res => res.json())
         .then(data => setRestaurants(data))
       }, [])
 
+    function handleClick(e) {
+        console.log('click')
+        e.currentTarget.disabled = true
+        setIsClicked((isClicked) => !isClicked)
+    }
         
     return (
         <div>
@@ -20,6 +28,9 @@ function Restaurants() {
                     restaurantinfo={restaurant} />
                 })
             }
+
+            {<button onClick={handleClick}>add new</button>}
+            { isClicked ? <NewRestaurant /> : null}
         </div>
     )
 }
