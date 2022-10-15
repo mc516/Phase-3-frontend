91e293edc6a4921205edeed8e1c9cbb3a0ec7f34
Mickey <mickeyc11111@gmail.com>
2022-10-14 22:22:31 -0400

    fixed expandable comments but need to click on image

--- src/RestaurantCard.js
+++ src/RestaurantCard.js
@@ -8,15 +8,16 @@ function RestaurantCard( {restaurantinfo, reviews, clients, handleUpdateReview, 
 
     function handleClick(){
         setIsClicked((isClicked) => !isClicked)
-        console.log(isClicked)
+       
     }
     
 
     return (
-        <div className="restaurant-card" onClick={handleClick}>
+        <div className="restaurant-card">
             <img  
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_ZB1r0oUYrLuIYDFCt_yLMqFnC3Lh2PDYsw&usqp=CAU"
                 alt={name}
+                onClick={handleClick}
             />
             <div><h2>{name}</h2></div>
             <div><h3>{location}</h3></div>
