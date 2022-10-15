558d9b76990c6254b9f42f08ef0aa44160d01230
Mickey <mickeyc11111@gmail.com>
2022-10-14 22:20:24 -0400

    expand and collapse comments for each restaurant

--- src/RestaurantCard.js
+++ src/RestaurantCard.js
@@ -4,11 +4,16 @@ import { useState } from "react"
 
 function RestaurantCard( {restaurantinfo, reviews, clients, handleUpdateReview, addReview, handleDeleteReview} ) {
     const {name, location} = restaurantinfo
-   
+    const [isClicked, setIsClicked] = useState(false)
+
+    function handleClick(){
+        setIsClicked((isClicked) => !isClicked)
+        console.log(isClicked)
+    }
     
 
     return (
-        <div className="restaurant-card">
+        <div className="restaurant-card" onClick={handleClick}>
             <img  
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_ZB1r0oUYrLuIYDFCt_yLMqFnC3Lh2PDYsw&usqp=CAU"
                 alt={name}
@@ -16,7 +21,9 @@ function RestaurantCard( {restaurantinfo, reviews, clients, handleUpdateReview, 
             <div><h2>{name}</h2></div>
             <div><h3>{location}</h3></div>
 
-            {<Reviews reviews={reviews} restaurantinfo={restaurantinfo} clients={clients} handleUpdateReview={handleUpdateReview} addReview={addReview} handleDeleteReview={handleDeleteReview}/>}
+            { 
+                isClicked ? <Reviews reviews={reviews} restaurantinfo={restaurantinfo} clients={clients} handleUpdateReview={handleUpdateReview} addReview={addReview} handleDeleteReview={handleDeleteReview}/> : null
+            }
 
             
 
