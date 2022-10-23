bca1086fa91dbb8687d7084b4d2a50a2db36d025
Mickey <mickeyc11111@gmail.com>
2022-10-23 10:08:58 -0400

    added image to home, edit readme

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ function Home() {
     return (
         <div className="home">
             <h1>Home</h1>
+            <img className="restaurant-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnZuvSTJYn2PrvMw3_EspJmRDpe36nTUreSw&usqp=CAU" alt="restaurant"/>
 
         </div>
     )
